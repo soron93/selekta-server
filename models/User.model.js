5b70a9ee8acc7d5387656d06a13f7dc3a8970519
emmyuxui <emmyuxui@gmail.com>
2021-07-21 15:29:53 +0200

    added user model

--- models/User.model.js
+++ models/User.model.js
@@ -1,12 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  
+  password: String,
+
   username: {
     type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
-  },
-  password: String,
+    require: true,
+  }, 
+
+  userID: { //The user’s Spotify user ID,  do we need to have this in the user model?  sign in with spotify
+    type: String,
+    require: true,
+  }, 
+
+  userPlayList: [{type: Schema.Types.ObjectId, ref: 'PlayList'}]
 });
 
 const User = model("User", userSchema);
