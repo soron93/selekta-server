76a185e6b64d05f4071baba8a897fd56def03d64
beta <salvador.orona@gmail.com>
2021-07-22 03:43:33 +0200

    changed jsx to js and tested out logic

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ require("dotenv/config");
 // ℹ️ Connects to the database
 require("./db");
 
+
+
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
@@ -14,13 +16,46 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+
+
+// set up connect-mangodb
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+app.use(session({
+    secret: 'NotMyAge',
+    saveUninitialized: false, 
+    resave: false, 
+    cookie: {
+      maxAge: 1000*60*60*24// is in milliseconds.  expiring in 1 day
+    },
+    store: new MongoStore({
+    //   mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/ReactTodos",
+      mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/todo-app",
+      ttl: 60*60*24, // is in seconds. expiring in 1 day
+    })
+}));
+
+
+
+
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
+// const todoRoutes = require("./routes/todo.routes");
+// app.use("/api", todoRoutes);
+
 const authRoutes = require("./routes/auth.routes");
 app.use("/api", authRoutes);
+
+const fileUploadRoutes = require('./routes/file-upload.routes')
+app.use("/api",fileUploadRoutes);
+
+// const stripeRoutes = require('./routes/stripe.routes')
+// app.use("/api",stripeRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
---
+++ routes/file-upload.routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router  = express.Router();
+
+// include CLOUDINARY:
+const uploader = require('../middlewares/cloudinary.config.jsx');
+
+// ensure you have an input type like this in your hbs file
+/*
+<form method="POST" action="/upload" enctype="multipart/form-data">
+    <input type="file" name="imageUrl" accept="image/png, image/jpg">
+    <button type="submit">Submit</button> 
+</form>     
+*/
+
+// imageUrl is the input name in your hbs file
+
+
+router.post('/upload', uploader.single("imageUrl"), (req, res, next) => {
+     console.log('file is: ', req.file)
+    if (!req.file) {
+      next(new Error('No file uploaded!'));
+      return;
+    }
+    //You will get the image url in 'req.file.path'
+    //store that in the DB 
+    res.status(200).json({
+        image: req.file.path
+    }) 
+})
+
+module.exports = router;
+
