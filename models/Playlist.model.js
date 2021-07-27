ad3438eb1e6aeb9954af9d11a8ebf7ecdd8504d4
emmyuxui <emmyuxui@gmail.com>
2021-07-27 16:37:31 +0200

    server to schema

---
+++ models/Playlist.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+require ("./Tracks.model")
+require ("./User.model")
+
+// 1. Define your schema
+let PlaylistSchema = new Schema({
+  name: String, // dont know how to fix this to accept just a list of genres 
+  
+    tracks: [{
+       type: Schema.Types.ObjectId,
+       ref: "Track"
+    }],
+
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user"
+    }
+  }
+ 
+)
+
+// 2. Define your model
+let PlaylistModel = model('Playlist', PlaylistSchema)
+
+// 3. Export your Model with 'module.exports'
+module.exports = PlaylistModel
